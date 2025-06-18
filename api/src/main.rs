89af6525335ff3f51b34a552c21fae364456f709
Shashank <shashank.jangid17@gmail.com>
2025-06-18 21:55:01 +0530

    updates

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,22 +1,38 @@
-use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+use poem::{get, handler, listener::TcpListener, post, web::{Json, Path}, Route, Server};
+use store::Store;
+pub mod request_inputs;
+pub mod request_outputs;
 
-#[handler]
-fn hello(Path(name): Path<String>) -> String {
-    format!("Hello:{}", name)
-}
+use request_inputs::CreateWebsiteInput;
+use request_outputs::CreateWebsiteOutput;
 
 #[handler]
 fn root() -> String {
     format!("Server is Up and Running")
 }
 
+#[handler]
+fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+    // let s = Store{};
+    let id = s.create_website();
+    let response = CreateWebsiteOutput {
+        id: id
+    };
+    Json(response)
+}
+
+#[handler]
+fn get_website(Path(id): Path<String>) -> String {
+    format!("")
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
     .at("/", get(root))
-    .at("/hello/:name", get(hello));
+    .at("/website", post(create_website));
     // creates and run the http server
     Server::new(TcpListener::bind("0.0.0.0:3000"))
     .run(app)
     .await
-} 
+}
